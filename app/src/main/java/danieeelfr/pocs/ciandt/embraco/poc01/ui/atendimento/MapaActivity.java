192186ea0be56789a0d2330ae16349107874e4b1
DanielFR <danielfr@ciandt.com>
2017-03-17 15:30:27 -0300

    Inclusão da activity de mapa.

--- app/src/main/java/danieeelfr/pocs/ciandt/embraco/poc01/ui/atendimento/MapaActivity.java
+++ app/src/main/java/danieeelfr/pocs/ciandt/embraco/poc01/ui/atendimento/MapaActivity.java
@@ -1,10 +1,19 @@
 package danieeelfr.pocs.ciandt.embraco.poc01.ui.atendimento;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,50 +22,78 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.List;
+
 import danieeelfr.pocs.ciandt.embraco.poc01.R;
+import danieeelfr.pocs.ciandt.embraco.poc01.ui.home.HomeActivity;
 
 public class MapaActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private LatLng latLng;
+    boolean isGPSEnabled = false;
+    boolean isNetworkEnabled = false;
+    boolean canGetLocation = false;
+    Location location; // location
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 meters
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1; // 1 minute
+    protected LocationManager locationManager;
+    LocationManager mLocationManager;
+    Context context;
+    private Button b_get;
+    private GpsTracker gps;
+    double longitude;
+    double latitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapa);
 
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        gps.stopUsingGPS();
+    }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        gps = new GpsTracker(MapaActivity.this);
+
+        if(gps.canGetLocation()) {
+            location = gps.getLocal(MapaActivity.this);
+
+            longitude = gps.getLongitude();
+            latitude = gps.getLatitude();
+
+            location.setLatitude(latitude);
+            location.setLongitude(longitude);
+
+            latLng = new LatLng(latitude, longitude);
+        }
+        else
+        {
+            gps.showSettingsAlert();
+        }
+
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-
-        MarkerOptions o = new MarkerOptions();
-
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         mMap.setTrafficEnabled(true);
 
+        MarkerOptions options = new MarkerOptions()
+                .position(latLng)
+                .title("Você está aqui!");
+
+        mMap.addMarker(options);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(16), 2000, null);
     }
 }
